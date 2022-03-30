690f20321e5997bf81caf0a232c57e6857b99752
cobu2021 <practicascarbajal2021.gmail.com>
2022-03-30 16:22:40 -0500

    correcciones

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
-from pickle import FALSE
+from datetime import timedelta
 from flask import Flask , render_template
 from flask_restful import Api
-from controllers.usuarios import RegistroController ,LoginController
+from controllers.usuarios import RegistroController ,LoginController 
+from controllers.movimientos import MovimientoController
+
 from config import validador, conexion
 from os import environ
 from dotenv import load_dotenv
+from flask_cors import CORS
+from flask_jwt import JWT , jwt_required , current_identity
+from seed import categoriaSeed
+from seguridad import autenticador, identificador 
+from dto.registro_dto import UsuarioResponseDTO
+
 
 load_dotenv()
 
 app = Flask(__name__)
+
+CORS (app=app)
+app.config['SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+#para cambiar el endpoint de mi JWT
+app.config['JWT_AUTH_URL_RULE'] = '/login-jwt'
+#para cambiar la llave para solicitar el username
+app.config['JWT_AUTH_USERNAME_KEY'] = 'correo'
+#para cambiar la llave para solicitar el password
+app.config['JWT_AUTH_PASSWORD_KEY'] = 'pass'
+#para cambiar el tiempo de expiracion de mi jwt
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(hours=1 , minutes=5)
+app.config['JWT_AUTH_HEADER_PREFIX'] = 'Bearer'
+
+
+jsonwebtoken = JWT(app=app, authentication_handler=autenticador,
+                    identity_handler=identificador)
 
 api = Api(app=app)
 validador.init_app(app)
@@ -17,8 +41,29 @@ conexion.init_app(app)
 # conexion.drop_all(app=app)
 conexion.create_all(app=app)
 
+#ingresara antes de hacer el primer request a nuestra funcion, toda la logica que queramos
+#que se haga antes de la primera solicitud la deberemos de colocar aqui
+
+@app.before_first_request
+def seed():
+    # ahora hacemos el seed de las tablas respectivas
+    categoriaSeed()
+
 @app.route('/')
 def inicio():
+    pass
+@app.route('/yo')
+@jwt_required()
+def perfil_usuario():
+    print(current_identity)
+    #serializar el usuario(current Identity)
+    usuario = UsuarioResponseDTO().dump(current_identity)
+    return {
+        'messge' : ' El usuario es',
+        'content' : usuario
+    }
+
+
     #render_template > renderiza un archivo .html o .jinja para flask lo que pueda leer e
     #interpretar al cliete
     return render_template('inicio.jinja', nombre='Eduardo', dia='Jueves' , integrantes=[
--- controllers/usuarios.py
+++ controllers/usuarios.py
@@ -1,8 +1,9 @@
-from ast import arg
 from flask_restful import Resource , request 
-from dto.regisro_dto import RegistroDTO , UsuarioResponseDTO , LoginDTO
+from dtos.registro_dto import RegistroDTO , UsuarioResponseDTO , LoginDTO
+from dtos.usuario_dto import ResetPasswordRequestDTO
 from models.usuarios import Usuario
-from config import conexion
+from config import conexion, sendgrid
+from sendgrid.helpers.mail import Email, To, Content, Mail
 
 
 
@@ -23,6 +24,7 @@ class RegistroController(Resource):
             'content' : respuesta
             },201
         except Exception as e:
+            conexion.session.rollback()
             return {
                 'message' : 'Error al registrar usuario',
                 'content': e.args
@@ -44,3 +46,38 @@ class LoginController(Resource):
                 'content' : e.args
             }
 
+
+class ResetPasswordController(Resource):
+    def post(self):
+        body = request.get_json()
+        try:
+            data = ResetPasswordRequestDTO().load(body)
+            # validar si existe ese usuario en mi bd
+            usuarioEncontrado = conexion.session.query(
+                Usuario).filter_by(correo=data.get('correo')).first()
+            if usuarioEncontrado is not None:
+                # tengo que utilizar los correos verificados en sendgrid ya que si uso uno que no esta verificado entonces el correo nunca llegara
+                from_email = Email('ederiveroman@outlook.com')
+                print(usuarioEncontrado.correo)
+                from_email = Email('ederiveroman@gmail.com')
+                to_email = To(usuarioEncontrado.correo)
+                subject = 'Reinicia tu contraseña del Monedero App'
+                content = Content(
+                    'text/plain', 'Hola, has solicitado el reinicio de tu contraseña, haz click en el siguiente link para cambiar, sino has sido tu ignora este mensaje: ....')
+                mail = Mail(from_email, to_email, subject, content)
+                envia_correo = sendgrid.client.mail.send.post(
+                    request_body=mail.get())
+                # el estado de la respuesta de sendgrid
+                print(envia_correo.status_code)
+                # el cuerpo de la respuesta de sendgrid
+                print(envia_correo.body)
+                # las cabeceras de la respuesta de sendgrid
+                print(envia_correo.headers)
+            return {
+                'message': 'Correo enviado exitosamente'
+            }
+        except Exception as e:
+            return {
+                'message': 'Error al resetear la password',
+                'content': e.args
+            }
